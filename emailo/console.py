71c1c49a9161710d363aa0342fb0a007bc190424
Vlad Dmitrievich <me@2tunnels.com>
2020-02-06 00:41:04 +0200

    add domains command

--- emailo/console.py
+++ emailo/console.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 import click
 
 from emailo.utils import parse_emails
@@ -19,3 +21,26 @@ def parse(file: str) -> None:
                 click.echo(email)
 
     click.echo("Hello, world!")
+
+
+@app.command()
+@click.argument("file", type=click.Path(exists=True))
+def domains(file: str) -> None:
+    counter = Counter()
+    max_domain_length = 0
+
+    with open(file, mode="r", encoding="utf-8") as f:
+        for line in f:
+            domain = line.strip().split("@")[1]
+            domain_length = len(domain)
+
+            if domain_length > max_domain_length:
+                max_domain_length = domain_length
+
+            counter[domain] += 1
+
+    for domain, count in sorted(
+        counter.items(), key=lambda items: items[1], reverse=True
+    ):
+        formatted_domain = domain.ljust(max_domain_length)
+        click.echo(f"{formatted_domain} {count}")
